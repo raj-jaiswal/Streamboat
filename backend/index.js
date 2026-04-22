6e62424e742178ce661a44eff02f85b0f6926ff3
Phoenix1729398 <shivamab782@gmail.com>
2026-04-22 19:16:30 +0530

    cors wala change not required

--- backend/index.js
+++ backend/index.js
@@ -9,15 +9,7 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: [
-    'http://localhost:5173', 
-    'https://streamboat.vercel.app'
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  credentials: true, // Important if your app relies on cookies or specific authorization headers
-}));
-
+app.use(cors());
 app.use(express.json());
 
 // Routes
@@ -28,11 +20,11 @@ app.use('/api/upload', require('./routes/uploadRoutes'));
 app.use('/api/copyright', require('./routes/claimRoutes'));
 
 app.get('/', (req, res) => {
-  res.send('Streamboat API is running...');
+  res.send('Streamboat API is running...');
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
