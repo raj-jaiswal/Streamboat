b047a8b7fc001cb7e31b2996cddc1b9dd8b02ce6
Phoenix1729398 <shivamab782@gmail.com>
2026-04-22 19:04:51 +0530

    cors wala change

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,15 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173', 
+    'https://streamboat.vercel.app'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true, // Important if your app relies on cookies or specific authorization headers
+}));
+
 app.use(express.json());
 
 // Routes
